4cac6481432beb11555255117ed331d0c86b0fde
CharlyCst <charly.castes@epfl.ch>
2020-02-19 18:54:00 +0100

    Cleaning up

--- node/profile.sh
+++ node/profile.sh
@@ -1,6 +1,6 @@
 # Using node
-NODE_ENV=production node --prof wasm.js 
-node --prof-process isolate-0x49a1aa0-11836-v8.log > wasm.txt
+# NODE_ENV=production node --prof wasm.js 
+# node --prof-process isolate-0x49a1aa0-11836-v8.log > wasm.txt
 
 
 # Using perf
