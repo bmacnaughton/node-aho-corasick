52adfb59959c311f000e98369ded68d3c6623f1a
Bruce A. MacNaughton <bruce.macnaughton@contrastsecurity.com>
2021-10-04 04:07:11 -0700

    get rid of leftovers

--- node/profile.sh
+++
@@ -1,8 +0,0 @@
-# Using node
-# NODE_ENV=production node --prof wasm.js 
-# node --prof-process isolate-0x49a1aa0-11836-v8.log > wasm.txt
-
-
-# Using perf
-perf record -g node --perf-basic-prof wasm.js
-perf report --no-children
