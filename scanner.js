e9e7529996493f694ebe14a07649273eca093995
Bruce A. MacNaughton <bruce.macnaughton@contrastsecurity.com>
2021-10-07 20:06:41 -0700

    turn scanner into chicken test

--- scanner.js
+++ scanner.js
@@ -4,6 +4,8 @@ const {AhoCorasick} = require('./index.linux-x64-gnu.node');
 
 const testPatterns = 'he him his she her hers '.split(' ').join('\x00');
 
+const testData = 'bruce said hers was awesome';
+
 class Scanner {
   constructor(array) {
     const arg = array.slice();
@@ -12,8 +14,13 @@ class Scanner {
     this.aho = new AhoCorasick(Buffer.from(arg.join('\x00')));
   }
 
-  isSuspicious(buffer) {
-    const indexes = this.aho.suspicious(buffer);
+  isSuspicious(bufferOrString) {
+    if (typeof bufferOrString === 'string') {
+      bufferOrString = Buffer.from(bufferOrString);
+    } else if (!Buffer.isBuffer(bufferOrString)) {
+      throw new Error('argument must be a buffer or string');
+    }
+    const indexes = this.aho.suspicious(bufferOrString);
     if (!indexes) {
       return indexes;
     }
@@ -24,4 +31,22 @@ class Scanner {
   }
 }
 
-module.exports = Scanner;
+module.exports = {
+  Scanner,
+  testPatterns,
+  testData,
+};
+
+if (require.main) {
+  const s = new Scanner('he him his she her hers'.split(' '));
+  const r = s.isSuspicious(testData);
+  if (!r) {
+    console.log('pattern not found');
+    process.exit(1);
+  }
+  if (r[0] != 'he' || r[1] !== 'her' || r[2] !== 'hers') {
+    console.log('r', r, 'not equal to testData', testData);
+    process.exit(1);
+  }
+  console.log('tests passed');
+}
